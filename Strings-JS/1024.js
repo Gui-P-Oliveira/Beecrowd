3b228eaa824ac74d69bdd0d77e58ff48a4de6343
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2024-01-12 17:39:43 -0300

    feat: create exercise 1024.js from Strings

---
+++ Strings-JS/1024.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+let N = parseInt(lines.shift());
+console.log
+
+for(let i = 0; i < N; ++i){
+    let M = lines.shift().trim().split('');
+
+    M = M.map((x) => x.match(/[A-Za-z]/i) ? String.fromCharCode(x.charCodeAt(0) + 3) : x);
+    M.reverse();
+    let metade = parseInt(Math.floor(M.length/2));
+    M = M.map((x, i) => i < metade ? x : String.fromCharCode(x.charCodeAt(0) - 1));
+
+    let resposta = M.join('');
+
+    console.log(resposta);
+}
